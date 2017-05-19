bfd7aee20eaf9574fed0e941800db9b51b11bcac
James Valentine <jv@f13dev.com>
2017-05-19 09:54:48 +0100

    formatting main index

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// Load the settings file
+require_once('inc/settings.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <header>
+    <h1>Photo archive</h1>
+    <div class="right">
+      Viewing: /<?php // Get image path ?><br />
+      <?php // get number of files ?> files
+    </div>
+  </header>
+  <section id="fileManager">
+
+  </section>
+  <section id="imageBrowser">
+    <div class="center">
+      Copyright &copy; <?php // get copyright holder ?> (<?php echo date("Y"); ?>) - Powered by: <a href="http://f13dev.com">PHP Photo Archive</a>
+    </div>
+  </section>
+  <footer>
+
+  </footer>
+</body>
+</html>
