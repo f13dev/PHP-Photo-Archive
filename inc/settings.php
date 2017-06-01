4334fcf77515168386f8615d4ab36cb44d368b23
James Valentine <jv@f13dev.com>
2017-06-01 10:00:47 +0100

    Commenting in settings

--- inc/settings.php
+++ inc/settings.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Define the CSS files to use
+ */
 define('CSS_MAIN','style.css');
 define('CSS_MOBILE','mobile.css');
 
+/**
+ * Define the images and thumbs directories,
+ * relative to the root of PHP-Photo-Archive.
+ */
 define('ARCHIVE_MAIN', 'archive/images/');
 define('ARCHIVE_THUMBS', 'archive/thumbs/');
