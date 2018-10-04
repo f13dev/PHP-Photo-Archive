0e5222d06ddee798006718bfb334eee88a5114e7
James Valentine <jv@f13dev.com>
2018-10-04 09:24:03 +0100

    Adding error reporting;

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// Error checking
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Load the settings file
 require_once('inc/settings.php');
 // Load utility classes
